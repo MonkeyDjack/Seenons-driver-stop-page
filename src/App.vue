641bf9303b783af2ffd43d88560f3033544d4f2a
Muhitdin Yahya <monkeydjack0@mail.ru>
2021-08-02 10:16:17 +0200

    created base vue app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Header/>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: rgb(50,126,134);
+  background: linear-gradient(90deg, rgba(50,126,134,1) 0%, rgba(76,193,149,1) 100%);
+}
+.container {
+  min-width: 320px;
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="header">
+        <div class="header-container">
+            <img src="../assets/img/avatar-empty.png" alt="avatar" class="avatar">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  
+}
+</script>
+
+<style scoped>
+.header-container{
+    display: flex;
+    justify-content: space-around;
+}
+.avatar{
+    width: 120px;
+}
+
+</style>
